a1a8d7648b3e1a154667d6dc55bc4fcace397663
Don Barlow <ottobar@gmail.com>
2018-10-04 15:14:42 -0600

    correct failing test

--- test/elm_on_phoenix_starter_app_web/controllers/page_controller_test.exs
+++ test/elm_on_phoenix_starter_app_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElmOnPhoenixStarterAppWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ ~s(<div id="elm-main">)
+    assert html_response(conn, 200) =~ ~s(<script src="/js/main.js">)
   end
 end
